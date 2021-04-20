7b4b106c931d4eaac43d7bf9bc5acfc1ad2edba6
Quy Nguyen <phuquy.uit@gmail.com>
2021-04-20 11:58:55 +0700

    udpate

--- src/app/modals/register/register.component.ts
+++ src/app/modals/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,6 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  regisForm = new FormGroup({
+    address: new FormControl(null),
+    email: new FormControl(null),
+    fullName: new FormControl(null),
+    gender: new FormControl(null),
+    idCard: new FormControl(null),
+    password: new FormControl(null),
+    phoneNumber: new FormControl(null),
+    roleId: new FormControl(null),
+  })
   constructor() { }
 
   ngOnInit() {
